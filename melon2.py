cb636b0dfc9ac5442b614aedb63cd0cf755e60a5
chae-ye <erica20050323@gmail.com>
2025-04-18 17:51:33 +0000

    멜론

--- melon2.py
+++ melon2.py
@@ -60,18 +60,6 @@ def search_artist_and_get_songs(artist_name):
     for i, title in enumerate(songs, 1):
         print(f"{i}. {title}")
 
-#csv
-import csv
-
-# CSV 파일 쓰기
-with open('output.csv', mode='w', newline='', encoding='utf-8') as file:
-    csv_writer = csv.writer(file)
-    # 헤더 작성
-    csv_writer.writerow(['순위', '제목', '가수'])
-    # 데이터 행 작성
-    csv_writer.writerow(['1', 30, 'New York'])
-    csv_writer.writerow(['2', 25, 'Los Angeles'])
-
 # 1. 멜론 100
 # 2. 멜론 50
 # 3. 멜론 10
